9983fb97219d903cfe4a9410d1f610cca14a210c
C20334466 <C20334466@mytudublin.ie>
2023-10-03 14:24:38 +0100

    Calculate Largest and smallest number

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-numbers = [1,2,3,4,5] 
-avg = sum(numbers) / len(numbers)
-print(f"avg is: {avg}")
+numbers = []  # List to store user-entered numbers
+largest = None
+smallest = None
+
+while True:
+    try:
+        user_input = float(input("Enter a number (or 'done' to finish): "))
+        numbers.append(user_input)
+
+        if largest is None or user_input > largest:
+            largest = user_input
+
+        if smallest is None or user_input < smallest:
+            smallest = user_input
+
+    except ValueError:
+        if user_input.lower() == "done":
+            break
+        else:
+            print("Invalid input. Please enter a number or 'done'.")
+
+# Calculate and display the average, largest, and smallest
+average = sum(numbers) / len(numbers)
+print(f"Average: {average}")
+print(f"Largest: {largest}")
+print(f"Smallest: {smallest}"
